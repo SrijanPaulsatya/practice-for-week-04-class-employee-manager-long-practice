76783f754239a3aaf0d363eea48baf45c5b79f32
Srijan Paulsatya <pranshupaulsatya@gmail.com>
2024-10-25 14:26:17 +0530

    Initialises manager.js

---
+++ manager.js
@@ -0,0 +1,16 @@
+const Employee = require("./employee");
+
+class Manager extends Employee {
+	constructor(name, title, salary, manager = null, employees) {
+		super(name, title, salary, manager);
+		this.employees = [];
+	}
+
+	addEmployee(employee) {
+		this.employees.push(employee);
+	}
+
+}
+
+module.exports = Manager;
+
